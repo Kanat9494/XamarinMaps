bfbbb7e804402dcecb0db2650eda54bbdd3c1681
kana2022 <kkanat94@gmail.com>
2022-11-21 18:37:04 +0600

    Added map to main page

--- XamarinMaps/XamarinMaps/MainPage.xaml.cs
+++ XamarinMaps/XamarinMaps/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.GoogleMaps;
 
 namespace XamarinMaps
 {
@@ -13,6 +14,19 @@ namespace XamarinMaps
         public MainPage()
         {
             InitializeComponent();
+
+            Pin pinTokyo = new Pin()
+            {
+                Type = PinType.Place,
+                Label = "Tokyo SKYTREE",
+                Address = "Sumida-ku, Tokyo, Japan",
+                Position = new Position(35.71d, 139.81d),
+                Rotation = 33.3f,
+                Tag = "id_tokyo",
+            };
+
+            map.Pins.Add(pinTokyo);
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(pinTokyo.Position, Distance.FromMeters(5000))); 
         }
     }
 }

867cd4113bac0af9e26a927ef6d60e8ef8cb1a2b
kana2022 <kkanat94@gmail.com>
2022-11-21 18:32:36 +0600

    Added new packages

--- XamarinMaps/XamarinMaps.iOS/AppDelegate.cs
+++ XamarinMaps/XamarinMaps.iOS/AppDelegate.cs
@@ -4,6 +4,7 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using XamarinMaps.Constants;
 
 namespace XamarinMaps.iOS
 {
@@ -23,6 +24,7 @@ namespace XamarinMaps.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            Xamarin.FormsGoogleMaps.Init(AppConstants.GoogleMapsApiKey);
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
---
+++ XamarinMaps/XamarinMaps/Constants/AppConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinMaps.Constants
+{
+    public class AppConstants
+    {
+        public const string GoogleMapsApiKey = "";
+    }
+}

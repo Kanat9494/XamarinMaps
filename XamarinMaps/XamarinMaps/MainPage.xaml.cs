7befe3d3d69b63eb708074ce8d384f1de9d22334
kana2022 <kkanat94@gmail.com>
2022-11-24 14:32:37 +0600

    Added some new features

--- XamarinMaps/XamarinMaps/MainPage.xaml.cs
+++ XamarinMaps/XamarinMaps/MainPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Plugin.Geolocator;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.GoogleMaps;
 
@@ -11,6 +13,7 @@ namespace XamarinMaps
 {
     public partial class MainPage : ContentPage
     {
+        Position position;
         public MainPage()
         {
             InitializeComponent();
@@ -20,13 +23,27 @@ namespace XamarinMaps
                 Type = PinType.Place,
                 Label = "Дом, милый дом",
                 Address = "Ак-Орго, Бишкек, Кыргызстан",
-                Position = new Position(42.851889, 74.517327),
+                Position = position,
                 Rotation = 33.3f,
                 Tag = "id_bishkek",
             };
 
-            map.Pins.Add(pinTokyo);
-            map.MoveToRegion(MapSpan.FromCenterAndRadius(pinTokyo.Position, Distance.FromMeters(5000))); 
+            //map.Pins.Add(pinTokyo);
+            //map.MoveToRegion(new MapSpan(position, 2, 2)); 
+        }
+
+        async void GetLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.High);
+                var location = await Geolocation.GetLocationAsync(request);
+
+                //var centerMap = new Xamarin.Forms.GoogleMaps.Position(location.La)
+                position = new Position(location.Latitude, location.Longitude);
+            }
+            catch (Exception ex) { }
+            
         }
     }
 }

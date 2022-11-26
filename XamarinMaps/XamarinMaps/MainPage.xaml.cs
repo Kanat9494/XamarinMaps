da8a38dbf9b6311e0044ad467fd79e72700b9aba
Kanat9494 <kkanat94@gmail.com>
2022-11-26 21:46:02 +0600

    Already finished this project

--- XamarinMaps/XamarinMaps/MainPage.xaml.cs
+++ XamarinMaps/XamarinMaps/MainPage.xaml.cs
@@ -118,5 +118,35 @@ namespace XamarinMaps
         {
             return (float)headerNorthValue;
         }
+
+        void MapPinDragStart(object sender, PinDragEventArgs e)
+        {
+
+        }
+
+        async void MapPinDragEnd(object sender, PinDragEventArgs e)
+        {
+            var positions = new Position(e.Pin.Position.Latitude, e.Pin.Position.Longitude);
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(positions, Distance.FromMeters(2000)));
+
+            await App.Current.MainPage.DisplayAlert("Внимание", $"Ваше местоположение найдено: Ширина : " +
+                $"{e.Pin.Position.Latitude}, Долгота: {e.Pin.Position.Longitude}", "Ок");
+        }
+
+        void PickupButton_Clicked(object sender, EventArgs e)
+        {
+            Pin VehiclePins = new Pin()
+            {
+                Label = "Ваше местоположение",
+                Type = PinType.Place,
+                Icon = (Device.RuntimePlatform == Device.Android) ? BitmapDescriptorFactory.FromBundle("PickupPin.png") : BitmapDescriptorFactory.FromView(new Image() { Source = "PickupPin.png", WidthRequest = 30, HeightRequest = 30 }),
+                Position = new Position(42.851879, 74.517415),
+                IsDraggable = true
+            };
+
+            map.Pins.Add(VehiclePins);
+            var positions = new Position(42.851879, 74.517415);
+            map.MoveToRegion(MapSpan.FromCenterAndRadius(positions, Distance.FromMeters(1000)));
+        }
     }
 }
